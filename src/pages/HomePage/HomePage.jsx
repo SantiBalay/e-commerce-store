95611836eb5a10703705c5fb84a63d3dab41bd03
Santiago <santiagobalay1997@hotmail.com>
2020-02-10 18:03:18 -0300

    Primera actualizacion de homepage, casi terminado el css.

---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './HomePage.scss'
+import Directorio from '../../components/Directorio/Directorio';
+
+const HomePage = () => {
+    return (
+        <div className="homepage">
+                <Directorio/>
+        </div>
+
+
+    );
+};
+
+export default HomePage;
