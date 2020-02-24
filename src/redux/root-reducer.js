fafe609dd18b9404535b227219ee11d0c0c626fc
Santiago <santiagobalay1997@hotmail.com>
2020-02-24 11:19:43 -0300

    Agrego local storage con redux-persist para los items del cart. Del user se ocupa firebase

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,10 +1,21 @@
 import { combineReducers } from 'redux'
 
+import { persistReducer } from 'redux-persist'
+
+import storage from 'redux-persist/lib/storage' //localstorage
+
 import userReducer from './user/userReducer'
 import cartReducer from './cart/cartReducer'
 
+const persistConfig = {
+    key: 'root',
+    storage, //storage : lo que tenga storage
+    whitelist: ['cart'] //los reducers que quiero persistir
+}
 
-export default combineReducers({
+const rootReducer = combineReducers({
     user: userReducer,
     cart: cartReducer
 })
+
+export default persistReducer(persistConfig, rootReducer) //basicamente una version modificada del reducer con el config de la libreria. Orden superior
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,17 @@
 import {createStore, applyMiddleware} from 'redux'
 import logger from 'redux-logger'
 
+import { persistStore } from 'redux-persist'
+
 import rootReducer from './root-reducer'
 
 import { composeWithDevTools } from 'redux-devtools-extension';
 
 const middlewares = [logger] 
 
-const store = createStore(rootReducer, composeWithDevTools( applyMiddleware(...middlewares)))
+export const store = createStore(rootReducer, composeWithDevTools( applyMiddleware(...middlewares)))
 
-export default store;
+//voy a implementar local storage 
+export const persistor = persistStore(store) // es una version persistida digamos
+
+export default {store,persistor};
